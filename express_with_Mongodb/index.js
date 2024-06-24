c84573d1f80eed67b899d93e8e818e040aeef543
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-06-24 19:53:19 +0530

    CRUD

--- express_with_Mongodb/index.js
+++ express_with_Mongodb/index.js
@@ -1,20 +1,98 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 main()
 .then(()=>{
-    console.log("conneted mongo db")
+    console.log('connected')
 })
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/practice');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  await mongoose.connect('mongodb://127.0.0.1:27017/demo_practice');
 }
 
+//Database Schema
 const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    age:Number
+    Name: String,
+    Email : String,
+    age : Number
 })
-const User = mongoose.model('User',userSchema)
+
+//Make collections
+const User =  mongoose.model("User",userSchema)
+// const Employee =  mongoose.model("Employee",userSchema)
+
+//CRUD
+
+// const user2 = new User({
+//      Name:"Siyush",
+//      Email:"siyush@gmail.com",
+//      age:24
+// })
+// user2.save()
+// .then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+
+//isert Many
+//  User.insertMany([
+//     {
+//         "Name": "Alice",
+//         "Email": "alice@example.com",
+//         "Age": 28
+//     },
+//     {
+//         "Name": "Bob",
+//         "Email": "bob@example.com",
+//         "Age": 32
+//     },
+//     {
+//         "Name": "Charlie",
+//         "Email": "charlie@example.com",
+//         "Age": 25
+//     },
+//     {
+//         "Name": "Diana",
+//         "Email": "diana@example.com",
+//         "Age": 30
+//     },
+//     {
+//         "Name": "Ethan",
+//         "Email": "ethan@example.com",
+//         "Age": 27
+//     }
+// ]) 
+// .then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>console.log(err))
+
+
+//Update
+// User.updateOne({ Name: 'Siyush' }, { $set: { Name: 'Nikhil' } })
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// User.deleteOne({Name:'Nikhil'})
+// .then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+//Find
+// User.findById('6679791c6d656b7790d3fb4e')
+// .then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
